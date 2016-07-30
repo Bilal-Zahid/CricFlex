e61e520bca5b5266184065d757f8bc25a348e7e1
Bilal-Zahid <12bscsbzahid@seecs.edu.pk>
2016-07-29 18:24:22 -0700

    Welcome added.

---
+++ src/com/example/cricflex/PrefManager.java
@@ -0,0 +1,42 @@
+package com.example.cricflex;
+
+/**
+ * Created by Bilal-Zahid on 7/29/2016.
+ */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Lincoln on 05/05/16.
+ */
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "androidhive-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    //for just one time welcome screen change false with true
+    public boolean isFirstTimeLaunch() {
+        return true;/*pref.getBoolean(IS_FIRST_TIME_LAUNCH, false);*/
+    }
+
+}
