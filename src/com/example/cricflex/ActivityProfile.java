fac48b31cf3e1624727bca73e66a43c4a0ac58d9
Muhammad Asawal <12beemasawal@seecs.edu.pk>
2016-08-13 10:43:23 +0500

    Home layout added

---
+++ src/com/example/cricflex/ActivityProfile.java
@@ -0,0 +1,7 @@
+package com.example.cricflex;
+
+/**
+ * Created by Asawal on 8/11/2016.
+ */
+public class ActivityProfile {
+}
--- src/com/example/cricflex/FragmentHome.java
+++ src/com/example/cricflex/FragmentHome.java
@@ -10,11 +10,32 @@ public class FragmentHome extends Fragment {
 
     public FragmentHome(){}
 
+
+    private ArcProgress arcProgress;
+
+
+
+
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        arcProgress = (ArcProgress) rootView.findViewById(R.id.arc_progress);
+        arcProgress.setProgress(45);
 
         return rootView;
     }
