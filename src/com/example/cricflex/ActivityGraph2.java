3a41840d8f6399a08b4e3ef597daef4b81d981c1
Bilal-Zahid <12bscsbzahid@seecs.edu.pk>
2016-08-25 16:27:44 +0500

    Test Commit after adding WilliamCharts

---
+++ src/com/example/cricflex/ActivityGraph2.java
@@ -0,0 +1,13 @@
+package com.example.cricflex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityGraph2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph2);
+    }
+}
